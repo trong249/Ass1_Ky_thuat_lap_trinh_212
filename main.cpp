47daaf0835cbb12b666c0ffb4871e641933c3725
trong249 <59286861+trong249@users.noreply.github.com>
2022-01-30 20:14:35 +0700

    push all

--- main.cpp
+++ main.cpp
@@ -1,51 +1,161 @@
-/*
-    * Ho Chi Minh City University of Technology
-    * Faculty of Computer Science and Engineering
-    * Initial code for Assignment 0
-    * Programming Fundamentals Spring 2022
-    * Author: Tran Huy
-    * Date: 06.01.2022
-*/
+// /*
+//     * Ho Chi Minh City University of Technology
+//     * Faculty of Computer Science and Engineering
+//     * Initial code for Assignment 0
+//     * Programming Fundamentals Spring 2022
+//     * Author: Tran Huy
+//     * Date: 06.01.2022
+// */
 
-//The library here is concretely set, students are not allowed to include any other libraries.
+// //The library here is concretely set, students are not allowed to include any other libraries.
 
+// #include "studyInPink.h"
+// using namespace std;
+
+// int readFile(string filename, int& HP1, int& HP2, int& EXP1, int& EXP2, int& M1, int& M2, int& E1, int& E2, int& E3) {
+// //This function is used to read the input file,
+// //DO NOT MODIFY THIS FUNTION
+//     ifstream myfile(filename);
+//     if (myfile.is_open()) {
+//         myfile >> HP1 >> HP2;
+//         myfile >> EXP1 >> EXP2;
+//         myfile >> M1 >> M2;
+//         myfile >> E1 >> E2 >> E3;
+//         return 1;
+//     }
+//     else return 0;
+// }
+
+// int main(int argc, const char * argv[]) {
+//     string filename = "input.txt";
+//     int HP1 = -1, HP2 = -1;
+//     int EXP1 = -1, EXP2 = -1;
+//     int M1 = -1, M2 = -1;
+//     int E1 = -1, E2 = -1, E3 = -1;
+//     bool isRead = readFile(filename, HP1, HP2, EXP1, EXP2, M1, M2, E1, E2, E3);
+//     if (isRead){
+//         //Task 1
+//         cout << "***************** Task1 ******************"<<endl;
+//         cout << firstMeet(EXP1, EXP2, E1) <<" Update EXP1, EXP2"<< endl;
+//         cout << HP1 << " " << HP2 <<"         \\HP"<< endl;
+//         cout << EXP1 << " " << EXP2 <<"         \\EXP"<< endl;
+//         cout << M1 << " " << M2 <<"         \\M"<< endl;
+//         cout << E1 << " " << E2 << " " << E3 <<"     \\E"<< endl;
+//         //Task 2
+//         cout << "***************** Task2 ******************"<<endl;
+//         cout << investigateScene(EXP1, EXP2, HP2, M2, E2) <<" Update HP2, EXP1, EXP2, M2"<< endl;
+//         cout << HP1 << " " << HP2 <<"         \\HP"<< endl;
+//         cout << EXP1 << " " << EXP2 <<"         \\EXP"<< endl;
+//         cout << M1 << " " << M2 <<"         \\M"<< endl;
+//         cout << E1 << " " << E2 << " " << E3 <<"     \\E"<< endl;
+//         //Task 3
+//         cout << "***************** Task3 ******************"<<endl;
+//         cout <<traceLuggage( HP1,  EXP1,  M1,  E3) << "  Update HP1, EXP1, M1"<<endl;
+//         cout << HP1 << " " << HP2 <<"         \\HP"<< endl;
+//         cout << EXP1 << " " << EXP2 <<"         \\EXP"<< endl;
+//         cout << M1 << " " << M2 <<"         \\M"<< endl;
+//         cout << E1 << " " << E2 << " " << E3 <<"     \\E"<< endl;
+//     }
+//     else{
+//         cout << "Cannot read input file";
+//     }
+//     return 0;
+// }
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <fstream>
+#include <random>
+#include <time.h>
+#include <vector>
+#include <sstream>
 #include "studyInPink.h"
 using namespace std;
+//paste your code here
 
-int readFile(string filename, int& HP1, int& HP2, int& EXP1, int& EXP2, int& M1, int& M2, int& E1, int& E2, int& E3) {
-//This function is used to read the input file,
-//DO NOT MODIFY THIS FUNTION
-    ifstream myfile(filename);
-    if (myfile.is_open()) {
-        myfile >> HP1 >> HP2;
-        myfile >> EXP1 >> EXP2;
-        myfile >> M1 >> M2;
-        myfile >> E1 >> E2 >> E3;
-        return 1;
+void MyAssert(bool assertion, string msg)
+{
+    if (!assertion)
+    {
+        cout << msg << endl;
+        abort();
     }
-    else return 0;
 }
 
-int main(int argc, const char * argv[]) {
-    string filename = "input.txt";
-    int HP1 = -1, HP2 = -1;
-    int EXP1 = -1, EXP2 = -1;
-    int M1 = -1, M2 = -1;
-    int E1 = -1, E2 = -1, E3 = -1;
-    bool isRead = readFile(filename, HP1, HP2, EXP1, EXP2, M1, M2, E1, E2, E3);
-    if (isRead){
-        //Task 1
-        cout << firstMeet(EXP1, EXP2, E1) << endl;
-        cout << EXP1 << " "<< EXP2 << endl;
-        //Task 2
-        cout << investigateScene(EXP1, EXP2, HP2, M2, E2)<< endl;
-        cout << EXP1 << " "<< EXP2 << " " << HP2 << " " << M2 << endl;
-        //Task 3
-        cout << traceLuggage(HP1, EXP1, M1, E3)<< endl;
-        cout << HP1 << " "<< EXP1 << " " << M1 << endl;
+int main()
+{
+    fstream inp("studyInPinkINP.txt", ios_base::in);
+    fstream out("studyInPinkOUT.txt", ios_base::in);
+    int HP1, HP2, EXP1, EXP2, M1, M2, E1, E2, E3;
+    int hp1, hp2, exp11, exp12, exp21, exp22, exp31, m1, m2;
+    int result1, result2, result3;
+    int m, n;
+    string line1, line2;
+    vector<int> vec_input;
+    vector<int> vec_output;
+    while (!inp.eof())
+    {
+        getline(inp, line1);
+        stringstream ss(line1);
+        while (ss >> m)
+        {
+            vec_input.push_back(m);
+        }
     }
-    else{
-        cout << "Cannot read input file";
+    inp.close();
+    while (!out.eof())
+    {
+        getline(out, line2);
+        stringstream ss(line2);
+        while (ss >> n)
+        {
+            vec_output.push_back(n);;
+        }
     }
-    return 0;
+    out.close();
+    int l = 0;
+    int i = 1;
+    for (int k = 0; k < (int)vec_input.size();)
+    {
+        HP1 = vec_input[k];
+        HP2 = vec_input[k + 1];
+        EXP1 = vec_input[k + 2];
+        EXP2 = vec_input[k + 3];
+        M1 = vec_input[k + 4];
+        M2 = vec_input[k + 5];
+        E1 = vec_input[k + 6];
+        E2 = vec_input[k + 7];
+        E3 = vec_input[k + 8];
+        result1 = vec_output[l];
+        exp11 = vec_output[l + 1];
+        exp12 = vec_output[l + 2];
+        result2 = vec_output[l + 3];
+        exp21 = vec_output[l + 4];
+        exp22 = vec_output[l + 5];
+        hp2 = vec_output[l + 6];
+        m2 = vec_output[l + 7];
+        result3 = vec_output[l + 8];
+        hp1 = vec_output[l + 9];
+        exp31 = vec_output[l + 10];
+        m1 = vec_output[l + 11];
+        int res1 = firstMeet(EXP1, EXP2, E1);
+        MyAssert(res1 == result1, "Fail in testcase " + to_string(i) + ": Expected result1 = " + to_string(result1) + ", but res1 = " + to_string(res1) + " found");
+        MyAssert(EXP1 == exp11, "Fail in testcase " + to_string(i) + ": Expected EXP1 = " + to_string(exp11) + ", but EXP1 = " + to_string(EXP1) + " found");
+        MyAssert(EXP2 == exp12, "Fail in testcase " + to_string(i) + ": Expected EXP2 = " + to_string(exp12) + ", but EXP2 = " + to_string(EXP2) + " found");
+        int res2 = investigateScene(EXP1, EXP2, HP2, M2, E2);
+        MyAssert(res2 == result2, "Fail in testcase " + to_string(i) + ": Expected result2 = " + to_string(result2) + ", but res2 = " + to_string(res2) + " found");
+        MyAssert(EXP1 == exp21, "Fail in testcase " + to_string(i) + ": Expected EXP1 = " + to_string(exp21) + ", but EXP1 = " + to_string(EXP1) + " found");
+        MyAssert(EXP2 == exp22, "Fail in testcase " + to_string(i) + ": Expected EXP2 = " + to_string(exp22) + ", but EXP2 = " + to_string(EXP2) + " found");
+        MyAssert(HP2 == hp2, "Fail in testcase " + to_string(i) + ": Expected HP2 = " + to_string(hp2) + ", but HP2 = " + to_string(HP2) + " found");
+        MyAssert(M2 == m2, "Fail in testcase " + to_string(i) + ": Expected M2 = " + to_string(m2) + ", but M2 = " + to_string(M2) + " found");
+        int res3 = traceLuggage(HP1, EXP1, M1, E3);
+        MyAssert(res3 == result3, "Fail in testcase " + to_string(i) + ": Expected result3 = " + to_string(result3) + ", but res3 = " + to_string(res3) + " found");
+        MyAssert(HP1 == hp1, "Fail in testcase " + to_string(i) + ": Expected HP1 = " + to_string(hp1) + ", but HP1 = " + to_string(HP1) + " found");
+        MyAssert(EXP1 == exp31, "Fail in testcase " + to_string(i) + ": Expected EXP1 = " + to_string(exp31) + ", but EXP1 = " + to_string(EXP1) + " found");
+        MyAssert(M1 == m1, "Fail in testcase " + to_string(i) + ": Expected M1 = " + to_string(m1) + ", but M1 = " + to_string(M1) + " found");
+        cout << "OK! Testcase " << i << " passed" << endl;
+        i++;
+        k += 9;
+        l += 12;
+    }
 }
--- studyInPink.h
+++ studyInPink.h
@@ -27,247 +27,258 @@ using namespace std;
 
 int firstMeet(int& EXP1, int& EXP2, const int& E1){
     //Complete this function to gain point on task 1
+    if(E1<0 || E1>999)
+        return -999;
+    else{
+        // Case 1:
+        if( E1>=0 && E1<=399 ){
+            int case1=25;
+            int case2=50;
+            int case3=85;
 
-    // Case 1:
-    if( EXP1>=0 && EXP1<=399 ){
-        int case1=25;
-        int case2=50;
-        int case3=85;
+            if ( E1>=0 && E1<=49 ){
+                EXP2+=case1;
+            }
 
-        if ( E1>=0 && E1<=49 ){
-            EXP2+=case1;
-        }
+            if ( E1>=50 && E1<=99 ){
+                EXP2+=case2;
+            }
 
-        if ( E1>=50 && E1<=99 ){
-            EXP2+=case2;
-        }
+            if ( E1>=100 && E1<=149 ){
+                EXP2+=case3;
+            }
 
-        if ( E1>=100 && E1<=149 ){
-            EXP2+=case3;
-        }
+            if ( E1>=150 && E1<=199 ){
+                EXP2+=case1+case2;
+            }
 
-        if ( E1>=150 && E1<=199 ){
-            EXP2+=case1+case2;
-        }
+            if ( E1>=200 && E1<=249 ){
+                EXP2+=case1+case3;
+            }
 
-        if ( E1>=200 && E1<=249 ){
-            EXP2+=case1+case3;
-        }
+            if ( E1>=250 && E1<=299 ){
+                EXP2+=case2+case3;
+            }
 
-        if ( E1>=250 && E1<=299 ){
-            EXP2+=case2+case3;
-        }
+            if ( E1>=300 && E1<=399 ){
+                EXP2+=case1+case2+case3;
+            }   
 
-        if ( E1>=50 && E1<=99 ){
-            EXP2+=case1+case2+case3;
-        }   
-
-        if ( E1%2!=0 ){
-            EXP1=ceil((float)EXP1+(float)E1/10);
+            if ( E1%2!=0 ){
+                EXP1=ceil((float)EXP1+(float)E1/10);
+            }
+            else {
+                EXP1=ceil((float)EXP1-(float)E1/5);
+            }
         }
-        else {
-            EXP1=ceil((float)EXP1-(float)E1/5);
-        }
-    }
-    
-    // Case 2:
-    if( E1>=400 && E1<=999 ){
-        // information 1
-        if ( E1>=400 && E1<=499 ){
-            EXP2=ceil( (float)EXP2+(float)E1/7+9);
-        }
-        // information 2
-        if ( E1>=500 && E1<=599 ){
-            EXP2=ceil( (float)EXP2+(float)E1/9+11);
-        }
-        // information 3
-        if ( E1>=600 && E1<=699 ){
-            EXP2=ceil( (float)EXP2+(float)E1/5+6);
-        }
-        // information 4
-        if ( E1>=700 && E1<=799 ){
-            EXP2=ceil( (float)EXP2+(float)E1/7+9);
-            if(EXP2>200){
+        
+        // Case 2:
+        if( E1>=400 && E1<=999 ){
+            // information 1
+            if ( E1>=400 && E1<=499 ){
+                EXP2=ceil( (float)EXP2+(float)E1/7+9);
+            }
+            // information 2
+            if ( E1>=500 && E1<=599 ){
                 EXP2=ceil( (float)EXP2+(float)E1/9+11);
             }
-        }
-        // information 5
-        if ( E1>=800 && E1<=999 ){
-            EXP2=ceil( (float)EXP2+(float)E1/7+9);
-            EXP2=ceil( (float)EXP2+(float)E1/9+11);
-            if(EXP2>600){
+            // information 3
+            if ( E1>=600 && E1<=699 ){
                 EXP2=ceil( (float)EXP2+(float)E1/5+6);
-                EXP2=ceil( (float)EXP2*0.15 + (float)EXP2 );
             }
+            // information 4
+            if ( E1>=700 && E1<=799 ){
+                EXP2=ceil( (float)EXP2+(float)E1/7+9);
+                if(EXP2>200){
+                    EXP2=ceil( (float)EXP2+(float)E1/9+11);
+                }
+            }
+            // information 5
+            if ( E1>=800 && E1<=999 ){
+                EXP2=ceil( (float)EXP2+(float)E1/7+9+(float)E1/9+11);
+                if(EXP2>600){
+                    EXP2=ceil( (float)EXP2+(float)E1/5+6);
+                    EXP2=ceil( (float)EXP2*0.15 + (float)EXP2 );
+                }
+            }
+
+            EXP1=ceil( (float)EXP1-E1*0.1 );
         }
 
-        EXP1=ceil( (float)EXP1-(float)EXP1*0.1 );
-    }
+        if(EXP1>900) EXP1=900;
+        if(EXP1<0) EXP1=0;
 
-    if(EXP1>900) EXP1=900;
-    if(EXP1<0) EXP1=0;
-
-    if(EXP2>900) EXP2=900;
-    if(EXP2<0) EXP2=0;
-
-    return EXP1+EXP2;
+        if(EXP2>900) EXP2=900;
+        if(EXP2<0) EXP2=0;
+        return EXP1+EXP2;
+    } 
 }
 
 int investigateScene(int& EXP1, int& EXP2, int& HP2, int& M2, const int& E2){
     //Complete this function to gain point on task 2
-    // Phase 1
-    if ( E2>=0 && E2<=299){
-        // ring
-        EXP2=ceil( (float)EXP2+ (float)E2/9+10 );
-        EXP1=ceil( (float)EXP1+ ((float)E2/9+10)/3 );
-    }
-    if ( E2>=300 && E2<=499){
-        // ring
-        EXP2=ceil( (float)EXP2+ (float)E2/9+10 );
-        EXP1=ceil( (float)EXP1+ ((float)E2/9+10)/3 );
-        // jacket
-        EXP2=ceil( (float)EXP2+ (float)E2*0.35 );
-        EXP1=ceil( (float)EXP1+ (float)E2*0.35/3 );
-    }
-    if ( E2>=500 && E2<=999){
-        // ring
-        EXP2=ceil( (float)EXP2+ (float)E2/9+10 );
-        EXP1=ceil( (float)EXP1+ ((float)E2/9+10)/3 );
-        // jacket
-        EXP2=ceil( (float)EXP2+ (float)E2*0.35 );
-        EXP1=ceil( (float)EXP1+ (float)E2*0.35/3 );
-        // baggage
-        EXP2=ceil( (float)EXP2+ ( (float)E2/9+10+(float)E2*0.35 )*0.17 );
-        EXP1=ceil( (float)EXP1+ ( (float)E2/9+10+(float)E2*0.35 )*0.17/3 );
-    }
+    if(E2<0 || E2>999)
+        return -999;
+    else{
+            // Phase 1
+            if ( E2>=0 && E2<=299){
+                // ring
+                EXP2=ceil( (float)EXP2+ (float)E2/9+10 );
+                EXP1=ceil( (float)EXP1+ ((float)E2/9+10)/3 );
+                
+            }
+            if ( E2>=300 && E2<=499){
+                // ring
+                EXP2=ceil( (float)EXP2+ (float)E2/9+10 );
+                EXP1=ceil( (float)EXP1+ ((float)E2/9+10)/3 );
+                // jacket
+                EXP2=ceil( (float)EXP2+ (float)E2*0.35 );
+                EXP1=ceil( (float)EXP1+ (float)E2*0.35/3 );
+            }
+            if ( E2>=500 && E2<=999){
+                // ring
+                EXP2=ceil( (float)EXP2+ (float)E2/9+10 );
+                EXP1=ceil( (float)EXP1+ ((float)E2/9+10)/3 );
+                // jacket
+                EXP2=ceil( (float)EXP2+ (float)E2*0.35 );
+                EXP1=ceil( (float)EXP1+ (float)E2*0.35/3 );
+                // baggage
+                EXP2=ceil( (float)EXP2+ ( (float)E2/9+10+(float)E2*0.35 )*0.17 );
+                EXP1=ceil( (float)EXP1+ ( (float)E2/9+10+(float)E2*0.35 )*0.17/3 );
+            }
 
 
-    // Phase 2
-    HP2=ceil( (float)HP2 - (pow((float)E2,3) / pow(2,23)) );
-    if(E2%2==0){
-        M2 = ceil( (float)M2 + (pow((float)E2,2)/50) );
+            // Phase 2
+            HP2=ceil( (float)HP2 - (pow((float)E2,3) / pow(2,23)) );
+            if(E2%2==0){
+                M2 = ceil( (float)M2 + (pow((float)E2,2)/50) );
+            }
+
+            if(HP2<0) HP2=0;
+            if(EXP1>900) EXP1=900;
+            if(EXP2>900) EXP2=900;
+            if(M2>2000) M2=2000;
+            return EXP2+HP2+M2+EXP1;
     }
-
-    if(HP2<0) HP2=0;
-    if(EXP1>900) EXP1=900;
-    if(EXP2>900) EXP2=900;
-    if(M2>2000) M2=2000;
-
-    return EXP2+HP2+M2+EXP1;
+    
 }
 
 int traceLuggage(int& HP1, int& EXP1, int& M1, const int& E3){
     //Complete this function to gain point on task 3
-    // Road 1
+    if(E3<0 || E3>999)
+        return -999;
+    else {
+                // Road 1
+            int P1[9]={1, 3, 5, 7, 9, 11, 13, 15, 17};
+            bool checkRoad1 = false;
+            for(int i = 0; i < 9; i++){
+                P1[i] = ( P1[i] + E3 ) % 26 + 65;
+                if( P1[i] == 80 ){
+                    checkRoad1 = true;
+                    HP1 -= P1[i]*(i+1)*2;
+                    EXP1 += (1000-P1[i]*(i+1))%101;
+                    M1 = ceil( (float)M1-(float)E3*(i+1)/9);
+                    break;
+                }
+            }
+            if( checkRoad1==false ){
+                M1=ceil( (float)M1-(float)E3*9);
+            }
+                
 
-    int P1[9]={1, 3, 5, 7, 9, 11, 13, 15, 17};
-    bool checkRoad1 = false;
-    for(int i = 0; i < 9; i++){
-        P1[i] = ( P1[i] + E3 ) % 26 + 65;
-        if( P1[i] == 80 ){
-            checkRoad1 = true;
-            HP1 -= P1[i]*(i+1)*2;
-            EXP1 += (1000-P1[i]*(i+1))%101;
-            M1 = ceil( (float)M1-(float)E3*(i+1)/9);
-            break;
-        }
-    }
-    if( checkRoad1==false ){
-        M1=ceil( (float)M1-(float)E3*9);
-    }
-        
 
+            // // Road 2
+            int P2[7]={2,3,5,7,11,13,17};
+            int s=0,m=0;
+            bool checkRoad2=false;
+            for(int i=0;i<7;i++){
+                P2[i]=(P2[i]+E3)%26;
+                s+=P2[i];
+            }
+            m=ceil((float)s/7);
+            for (int i = 0; i < 7; i++)
+            {
+                P2[i] = (P2[i] + s + m)%26 + 65;
+                if( P2[i] == 80 ){
+                    checkRoad2 = true;
+                    HP1 -= P2[i]*(i+1)*2;
+                    EXP1 += (1000-P2[i]*(i+1))%101;
+                    M1 = ceil( (float)M1-(float)E3*(i+1)/9);
+                    break;
+                }
+            }
+            if( checkRoad2==false ){
+                M1=ceil( (float)M1-(float)E3*7*7/9);
+            }
+            
+            // Road 3
+            int P3[20]={4,16,36,64,100,144,196,256,324,400,484,576,676,784,900,1024,1156,1296,1444,1600};
+            bool checkRoad3=false;
+            int max=0;
+            for(int i = 0; i<20; i++){
+                P3[i]=(P3[i] + E3*E3)%113;
+                if(P3[i]>max){
+                    max=P3[i];
+                }
+            }
+            for(int i = 19; i>=0; i--){
+                P3[i]= ceil( (float)(P3[i]+E3)/max);
+                P3[i]=P3[i]%26 + 65;
 
-    // Road 2
-    int P2[7]={2,3,5,7,11,13,17};
-    int s=0,m=0;
-    bool checkRoad2=false;
-    for(int i=0;i<7;i++){
-        s+=P2[i];
-        P2[i]=(P2[i]+E3)%26;
-    }
-    m=ceil(s/7);
-    for (int i = 0; i < 7; i++)
-    {
-        P2[i] = (P2[i] + s + m)%26 + 65;
-        if( P2[i] == 80 ){
-            checkRoad2 = true;
-            HP1 -= P2[i]*(i+1)*2;
-            EXP1 += (1000-P2[i]*(i+1))%101;
-            M1 = ceil( (float)M1-(float)E3*(i+1)/9);
-            break;
-        }
-    }
-    if( checkRoad2==false ){
-        M1=ceil( (float)M1-(float)E3*7*7/9);
-    }
-    
-    // Road 3
-    int P3[20]={4,16,36,64,100,144,196,256,324,400,484,576,676,784,900,1024,1156,1296,1444,1600};
-    bool checkRoad3=false;
-    int max=0;
-    for(int i = 0; i<20; i++){
-        P3[i]=(P3[i] + E3*E3)%113;
-        if(P3[i]>max){
-            max=P3[i];
-        }
-    }
-    for(int i = 19; i>=0; i--){
-        P3[i]= ceil( (P3[i]+E3)/max);
-        P3[i]=P3[i]%26 + 65;
+                if( P3[i] == 80 ){
+                    checkRoad3 = true;
+                    HP1 -= P3[i]*(20-i)*3;
+                    EXP1 += (3500-P3[i]*(20-i))%300;
+                    M1 = ceil( (float)M1-(float)E3*(20-i)/9);
+                    break;
+                }
+            }
+            if( checkRoad3==false ){
+                M1=ceil( (float)M1-(float)E3*20*20/9);
+            }
 
-        if( P3[i] == 80 ){
-            checkRoad3 = true;
-            HP1 -= P3[i]*(20-i)*3;
-            EXP1 += (3500-P3[i]*(20-i))%300;
-            M1 = ceil( (float)M1-(float)E3*(20-i)/9);
-            break;
-        }
-    }
-    if( checkRoad3==false ){
-        M1=ceil( (float)M1-(float)E3*20*20/9);
-    }
+            // Road 4
+            int P4[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+            bool checkRoad4=false;
+            int min=999,min_idx=0;
+            for(int i=0; i<12;i++){
 
-    // Road 4
-    int P4[12]={31,28,31,30,31,30,31,31,30,31,30,31};
-    bool checkRoad4=false;
-    int min=999,min_idx=0;
-    for(int i=0; i<12;i++){
-        int a=ceil( pow( (float)E3/29,3 ) );
-        P4[i] = ( P4[i] +  a) % 9;
-        if(P4[i]<min){
-            min=P4[i];
-            min_idx=i;
-        }
-    }
-    for(int i=11; i>=0; i--){
-        int a=ceil( (float)(P4[i]+E3)*(min/min_idx) );
-        P4[i]= a%26+65;
-        if( P4[i] == 80 ){
-            checkRoad4 = true;
-            HP1 -= P4[i]*(12-i)*4;
-            EXP1 += (4500-P4[i]*(12-i))%400;
-            M1 = ceil( (float)M1-(float)E3*(12-i)/9);
-            break;
-        }
-    }
-    if( checkRoad4==false ){
-        M1=ceil( (float)M1-(float)E3*12*12/9);
-    }
+                int a=ceil( (float)E3/29 );
+                P4[i] = ( P4[i] +  a*a*a ) % 9;
+                if(P4[i]<min){
+                    min=P4[i];
+                    min_idx=i+1;
+                }
+            }
+            for(int i=11; i>=0; i--){
 
-    if(M1<0) M1=0;
+                int a=ceil( (float)min/min_idx);
 
-    if( !(checkRoad1||checkRoad2||checkRoad3||checkRoad4) ){
-        HP1-=(59*E3)%900;
-        EXP1-=(79*E3)%300;
-        if(EXP1<0) EXP1=0;
-        if(HP1<0) HP1=0;
-        return -1;
-    }
-    else{
-        if(HP1<0) HP1=0;
-        if(EXP1>900) EXP1=900;
-        return HP1+EXP1+M1;
+                P4[i]= ((P4[i]+E3)*a)%26+65;
+                if( P4[i] == 80 ){
+                    checkRoad4 = true;
+                    HP1 -= P4[i]*(12-i)*4;
+                    EXP1 += (4500-P4[i]*(12-i))%400;
+                    M1 = ceil( (float)M1-(float)E3*(12-i)/9);
+                    break;
+                }
+            }
+            if( checkRoad4==false ){
+                M1=ceil( (float)M1-(float)E3*12*12/9);
+            }
+
+            if(M1<0) M1=0;
+            if( !(checkRoad1||checkRoad2||checkRoad3||checkRoad4) ){
+                HP1-=(59*E3)%900;
+                EXP1-=(79*E3)%300;
+                if(EXP1<0) EXP1=0;
+                if(HP1<0) HP1=0;
+                return -1;
+            }
+            else{
+                if(HP1<0) HP1=0;
+                if(EXP1>900) EXP1=900;
+                return HP1+EXP1+M1;
+            }
     }
 }
 
